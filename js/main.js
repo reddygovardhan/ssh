848974f6b3463378ea28e1f0bc64aea2f7cdfede
reddygovardhan <reddygovardhanreddy@gmail.com>
2018-02-12 23:17:14 +0530

    final changes done

--- js/main.js
+++ js/main.js
@@ -7,5 +7,12 @@ $(document).ready(function () {
       draggable: true // Choose whether you can drag to open on touch screens
     });
 	$(".slider").slider();
+	$('nav ul a').click(function(){
+    $('nav ul a').removeClass("active");
+    $('nav ul a').removeClass("main-nav-active");
+    $(this).addClass("active");
+    $(this).addClass("main-nav-active");
+});
+	$('.carousel.carousel-slider').carousel({full_Width: true});
 
 });
